1916575703f39aa5eba1f5ae99691ad0e2e3f497
valentinecomic <82827293+valentinecomic@users.noreply.github.com>
2022-06-23 06:22:52 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -54,6 +54,32 @@
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
        
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+       
     //Disable dragging and right-clicking
     $("body").on("contextmenu",function(e){
      return false;
