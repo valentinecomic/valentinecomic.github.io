7d600c1d3ac31b969d643b9a99cdd6347aaae700
Lyfaeren <82827293+valentinecomic@users.noreply.github.com>
2023-02-17 03:25:42 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -89,25 +89,4 @@
      e.preventDefault()
    });
 
-   //Date of Birth
-   function getAge(birth) {
-   var today = new Date();
-   var curr_date = today.getDate();
-   var curr_month = today.getMonth() + 1;
-   var curr_year = today.getFullYear();
-
-   var pieces = birth.split('/');
-   var birth_date = pieces[0];
-   var birth_month = pieces[1];
-   var birth_year = pieces[2];
-
-   if (curr_month == birth_month && curr_date >= birth_date) return parseInt(curr_year-birth_year);
-   if (curr_month == birth_month && curr_date < birth_date) return parseInt(curr_year-birth_year-1);
-   if (curr_month > birth_month) return parseInt(curr_year-birth_year);
-   if (curr_month < birth_month) return parseInt(curr_year-birth_year-1);
-   }
-   
-   var age = getAge('08/07/2006');
-   alert(age);
-
 })(jQuery); // End of use strict
